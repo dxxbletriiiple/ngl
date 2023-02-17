3af027ad5f6613607a916cf6bfd60e547f9f4642
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-02-17 06:36:33 +0300

    add: post & post form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
-	declarations: [AppComponent, PostComponent],
+	declarations: [AppComponent, PostComponent, PostFormComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+	selector: 'app-post',
+	templateUrl: './post.component.html',
+	styleUrls: ['./post.component.scss'],
 })
 export class PostComponent {
+	x = 10;
 
+	changeX = (e: any) => {
+		console.log(e);
+		this.x += 1;
+	};
 }
